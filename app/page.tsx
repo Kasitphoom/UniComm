d266a7b70b70d4dc9adc591fba416e6cc6dc7734
3044035t <3044035t@student.gla.ac.uk>
2025-10-19 23:59:10 +0100

    feat: Add Small Logo SVG file to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,65 @@
 import Image from "next/image";
+import LoginForm from "@/components/login/loginForm";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div className="flex min-h-svh p-8">
+            <div className="flex-1 flex items-center justify-center">
+                <Image
+                    src="/images/login-image.svg"
+                    width={500}
+                    height={500}
+                    alt="login image"
+                    className="w-full"
+                />
+            </div>
+            <div className="flex-1 flex justify-center items-center">
+                <div className="flex flex-col max-w-[37.5rem] p-12 gap-8">
+                    <div className="flex flex-col gap-4">
+                        <Image
+                            src="/images/logos/Big Logo.svg"
+                            width={254}
+                            height={52}
+                            className="aspect-[127/26]"
+                            alt="UniComm Logo"
+                        />
+                        <p className="text-default-400">
+                            Log in to your account to access the UniComm platform
+                        </p>
+                    </div>
+                    <LoginForm />
+                    <div className="flex gap-4 items-center">
+                        <div className="w-full h-[1px] bg-default-300"/>
+                        <p className="text-default-400">OR</p>
+                        <div className="w-full h-[1px] bg-default-300"/>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex py-2 px-4 border border-default-300 rounded-lg items-center gap-2 cursor-pointer hover:bg-default-100 transition">
+                            <Image
+                                src="/images/logos/Google Logo.png"
+                                width={20}
+                                height={20}
+                                alt="Google Logo"
+                            />
+                            <p className="w-full text-default-400 text-center">
+                                Continue with Google
+                            </p>
+                        </div>
+                        <div className="flex py-2 px-4 border border-default-300 rounded-lg items-center gap-2 cursor-pointer hover:bg-default-100 transition">
+                            <Image
+                                src="/images/logos/Salesforce-logo.png"
+                                width={20}
+                                height={20}
+                                alt="Google Logo"
+                                className="w-auto h-[20px]"
+                            />
+                            <p className="w-full text-default-400 text-center">
+                                Continue with Salesforce
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    ); 
 }
---
+++ components/login/loginForm.tsx
@@ -0,0 +1,65 @@
+'use client'
+import { Button, Input } from "@heroui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+    email: yup.string().email("Invalid email format").required("Email is required"),
+    password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+});
+
+const LoginForm = () => {
+
+    const { handleSubmit, control } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
+    return (
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name="email"
+                control={control}
+                render={({field , fieldState: {invalid, error}}) => (
+                    <Input
+                        {...field}
+                        label="Email"
+                        type="email"
+                        isRequired
+                        validationBehavior="aria"
+                        isInvalid={invalid}
+                        errorMessage={error ? error.message : undefined}
+                        labelPlacement="outside"
+                        placeholder="example@email.com"
+                    />
+                )}
+            />
+            <Controller
+                name="password"
+                control={control}
+                render={({field , fieldState: {invalid, error}}) => (
+                    <Input
+                        {...field}
+                        label="Password"
+                        type="password"
+                        isRequired
+                        validationBehavior="aria"
+                        isInvalid={invalid}
+                        errorMessage={error ? error.message : undefined}
+                        labelPlacement="outside"
+                        placeholder="Enter your password"
+                    />
+                )}
+            />
+            <Button type="submit" color="secondary">
+                Log In
+            </Button>
+        </form>
+    )
+}
+
+export default LoginForm
