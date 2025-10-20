ed1ff8d49e81d8df29af3c07e3c623c5312ae0c4
3044035t <3044035t@student.gla.ac.uk>
2025-10-20 22:16:14 +0100

    feat: Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["http://localhost:4100", "https://local.kasitphoom.com"],
 };
 
 export default nextConfig;
