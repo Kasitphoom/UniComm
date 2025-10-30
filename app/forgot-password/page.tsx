662cef212291b2e1760da49f8866f2918fc487e5
3044035t <3044035t@student.gla.ac.uk>
2025-10-30 17:23:16 +0000

    feat: Enhance password reset flow with updated UI and metadata adjustments

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -3,7 +3,14 @@ import ResetPasswordRequestForm from "@/components/login/ResetPasswordRequestFor
 import ResetPasswordForm from "@/components/login/ResetPasswordForm";
 import prisma from "@/lib/prisma-main";
 import { ReferencePurpose } from "../generated/main/prisma";
+import { Metadata } from "next";
+import Link from "next/link";
 // import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+    title: "Reset Your Password | UniComm",
+    description: "UniComm is a next-generation Customer Communication Management (CCM) and Unified Communication Platform that lets enterprises design, orchestrate, and deliver personalized, compliant messages across email, SMS, push, chat, and print. Built for scalability, and cloud-native security.",
+};
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ ref?: string }> }) {
     const params = await searchParams;
@@ -39,15 +46,23 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ r
             <div className="flex-1 flex justify-center items-center">
                 <div className="flex flex-col max-w-[37.5rem] py-8 md:p-12 gap-8">
                     <div className="flex flex-col gap-4">
-                        <Image
-                            src="/images/logos/Big Logo.svg"
-                            width={254}
-                            height={52}
-                            className="aspect-[127/26]"
-                            alt="UniComm Logo"
-                        />
+                        <Link href="/">
+                            <Image
+                                src="/images/logos/Big Logo.svg"
+                                width={254}
+                                height={52}
+                                className="aspect-[127/26]"
+                                alt="UniComm Logo"
+                            />
+                        </Link>
+                        {
+                            ref && <h1 className="text-xl font-semibold">Reset Your Password</h1>
+                        }
                         <p className="text-default-400">
-                            Please enter your email registered with UniComm to receive a password reset link.
+                            {
+                                ref ? "Please enter your new password below to reset your password." : "Please enter your email registered with UniComm to receive a password reset link."
+                            }
+                            
                         </p>
                     </div>
                     {ref && !errorMsg ? (
