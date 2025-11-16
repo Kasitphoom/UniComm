614fa4ed2acdcb0b0c2547c4c75a643fa331f869
3044035t <3044035t@student.gla.ac.uk>
2025-11-16 15:54:55 +0000

    feat: add delete functionality to TemplateItemCard; include dropdown menu for template deletion

--- components/templates/TemplateItemCard.tsx
+++ components/templates/TemplateItemCard.tsx
@@ -1,10 +1,11 @@
 'use client'
+import { deleteTemplate } from '@/features/templates/templatesSlice'
 import { setSidebarOpen } from '@/features/ui/uiSlice'
 import { useAppDispatch } from '@/store/hooks'
 import { TemplateWithUser } from '@/types/template'
 import { timeDifferenceFormatter } from '@/utils/DateFormatter'
-import { Card, CardBody, CardFooter, Skeleton, User } from '@heroui/react'
-import { Dot } from 'lucide-react'
+import { Button, Card, CardBody, CardFooter, Dropdown, DropdownMenu, DropdownTrigger, DropdownItem, Skeleton, User } from '@heroui/react'
+import { Dot, EllipsisVertical, TrashIcon } from 'lucide-react'
 import Image from 'next/image'
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
@@ -19,12 +20,16 @@ const TemplateItemCard = ({ template }: { template: TemplateWithUser }) => {
         dispatch(setSidebarOpen(false))
     }
 
+    const onTemplateDelete = async (e: React.MouseEvent) => {
+        await dispatch(deleteTemplate(template.id))
+    }
+
     return (
         <Card className='h-70' shadow='sm' isPressable onPress={onCardClick}>
             <CardBody>
                 <Skeleton className='w-full h-full rounded-md'></Skeleton>
             </CardBody>
-            <CardFooter className='flex flex-col gap-2 items-start justify-between'>
+            <CardFooter className='relative flex flex-col gap-2 items-start justify-between'>
                 <p className='line-clamp-2 text-ellipsis overflow-hidden'>{ template.title }</p>
                 <div className='flex gap-2 items-center flex-wrap'>
                     <User
@@ -37,6 +42,18 @@ const TemplateItemCard = ({ template }: { template: TemplateWithUser }) => {
                     <Dot size={12} className='text-default-400' />
                     <p className='text-xs text-default-400'>{ timeDifferenceFormatter(new Date(template.updatedAt)) }</p>
                 </div>
+                <Dropdown>
+                    <DropdownTrigger>
+                        <Button size='sm' radius='full' variant='light' className='absolute top-2 right-2 w-fit'>
+                            <EllipsisVertical size={16} />
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu>
+                        <DropdownItem key="Delete" color="danger" className='text-danger' startContent={<TrashIcon size={16} />}>
+                            Delete
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
             </CardFooter>
         </Card>
     )
