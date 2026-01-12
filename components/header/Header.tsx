33f179d30cc618c07687e8c326e20ec6d51ae181
3044035t <3044035t@student.gla.ac.uk>
2026-01-12 17:50:09 +0000

    feat: restructure Header component and implement MobileSidebar for improved navigation

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { getBusinessData } from '@/query/businessQuery'
 import HeaderUser from './HeaderUser';
+import MobileSidebar from '../sidebar/MobileSideBar';
 
 const Header = async () => {
     const business = await getBusinessData();
 
     return (
-        <div className='flex gap-4 justify-end h-16 border-b border-default-200 items-center px-4 w-full sticky'>
-            {business.name}
-            <HeaderUser />
+        <div className='flex justify-between'>
+            <MobileSidebar />
+            <div className='flex gap-4 justify-end h-16 border-b border-default-200 items-center px-4 w-full sticky'>
+                {business.name}
+                <HeaderUser />
+            </div>
         </div>
     )
 }
---
+++ components/sidebar/MobileSideBar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+    Link,
+    Button,
+} from "@heroui/react";
+import { Navigations } from "./SideBar"
+import { usePathname } from "next/navigation";
+
+export default function MobileSidebar() {
+    const pathname = usePathname();
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    // Flatten navigation groups into a single array
+    const flattenedMenuItems = Navigations.flatMap((group) => group.items);
+
+    return (
+        <Navbar
+            onMenuOpenChange={setIsMenuOpen}
+            className="md:hidden border-b border-divider" // Only show on mobile/tablet
+            isBordered
+        >
+            <NavbarContent>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="sm:hidden"
+                />
+            </NavbarContent>
+
+            {/* The Sidebar Menu - Only triggers on mobile */}
+            <NavbarMenu className="pt-6">
+                {flattenedMenuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item.name}-${index}`}>
+                        <Link
+                            color={
+                                pathname.split('/')[1] === item.href.replace('/','') ? "secondary" : "default"
+                            }
+                            className="w-full text-lg py-2"
+                            href={item.href}
+                            size="lg"
+                        >
+                            {item.name}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
+}
