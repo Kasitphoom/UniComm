0600055d0cfb62033c34843cfed71269d27c5acc
3044035t <3044035t@student.gla.ac.uk>
2026-03-23 15:27:42 +0000

    feat: add @vercel/analytics package and integrate Analytics component in Providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,10 +5,12 @@ import { SessionProvider } from 'next-auth/react'
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from '@/store/store'
 import { UserProvider } from '@/components/providers/UserProvider'
+import { Analytics } from "@vercel/analytics/next"
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <HeroUIProvider>
+            <Analytics />
             <ToastProvider placement='top-right' toastProps={{
                 color: "secondary",
                 timeout: 2000,
